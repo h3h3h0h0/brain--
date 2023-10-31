d8f5935937ea3a42d34875d0b65a4d442c673dff
h4h4h1h1 <yx2wang@uwaterloo.ca>
2023-10-31 10:21:00 -0400

    interpreter header

--- interpreter.h
+++ interpreter.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <istream>
+#include <ostream>
+#include <thread>
+#include <queue>
+#include "cell.h"
+
+using namespace std;
+
+class BFMInterpreter {
+    string program;
+    void _run(istream in, ostream out, string prog); //the public run method calls this, which will be threaded
+    public:
+        BFMInterpreter();
+        ~BFMInterpreter();
+        void load(string prog);
+        void loadFile(string fn);
+        void run(istream in, ostream out);
+        void stop();
+};
